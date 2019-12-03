6d57afd2ea00ce51cc22d6451b404b6679096874
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-12-03 20:23:04 +0600

    Add project files.

---
+++ DynamicAssemblyInDomainLesson/CalculatorAssemblyLoadContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+
+namespace DynamicAssemblyInDomainLesson
+{
+    class CalculatorAssemblyLoadContext : AssemblyLoadContext
+    {
+        private AssemblyDependencyResolver _resolver;
+
+        public CalculatorAssemblyLoadContext(string mainAssemblyToLoadPath) : base(isCollectible: true)
+        {
+            _resolver = new AssemblyDependencyResolver(mainAssemblyToLoadPath);
+        }
+
+        protected override Assembly Load(AssemblyName name)
+        {
+            var assemblyPath = _resolver.ResolveAssemblyToPath(name);
+            if (assemblyPath != null)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ DynamicAssemblyInDomainLesson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DynamicAssemblyInDomainLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var domain = AppDomain.CurrentDomain;
+            Console.WriteLine();
+        }
+    }
+}
